5b2774d83fe4b0e9e04adf3729962961ac5c29af
MAYANK JOHARI <81777768+mayankmj@users.noreply.github.com>
2023-05-14 12:16:17 +0530

    Time: 401 ms (44.93%), Space: 143.3 MB (5.02%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    long long c(int i,vector<vector<int>>& q,vector<long long>&dp){
-        if(i>=q.size())return 0;
-        long long ans=0;
-        if(dp[i]!=-1)return dp[i];
-        ans = max(q[i][0]+c(i+q[i][1]+1,q,dp),c(i+1,q,dp));
-        dp[i]=ans;
-        return ans;
+    long long ans_finder(vector<vector<int>>&q , int i , vector<long long >&dp)
+    {
+        if(i>=q.size()) return 0;
+        long long opt1=0,opt2=0;
+        if(dp[i]!=-1) return dp[i];
+        else{
+        opt1=q[i][0]+ans_finder(q,i+q[i][1]+1,dp);
+        opt2=ans_finder(q,i+1,dp);
+        return dp[i] = max({opt1,opt2});
+         }
     }
     long long mostPoints(vector<vector<int>>& q) {
         int n=q.size();
-      vector<long long>dp(n,-1);
-      return c(0,q,dp);  
+        vector<long long>dp(n,-1);
+        return(ans_finder(q,0,dp));
+        // for
+        // for(auto &x:dp) cout<<x<<" ";
+        // return 1;
     }
 };
